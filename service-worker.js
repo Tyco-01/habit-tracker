f74884e3cf0da7f9c4485303a5c492b6c38ddedb
Tyco-01 <deathboy18.top@gmail.com>
2026-08-21 18:39:26 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
 // có thể bị "kẹt" lại rất lâu dù đã tải code mới lên server.
 // ============================================================
 
-const CACHE_NAME = 'habit-tracker-v55';
+const CACHE_NAME = 'habit-tracker-v56';
 
 const CORE_ASSETS = [
   './',
